05d031bdfe797d2f4603bffd327d0e2202e2d1f1
AcruxPDE2 <martin.sanchez@acrux.mx>
2018-10-31 10:38:52 -0600

    Correccion de pruebas para evaluación 31/10/2018
    Pruebas Factores adicionales e ingles

---
+++ SistemaSIGEIN/SIGE.Entidades/IntegracionDePersonal/E_PRUEBAS_EVALUACION.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGE.Entidades.IntegracionDePersonal
+{
+    [Serializable]
+    public class E_PRUEBAS_EVALUACION
+    {
+        public int ID_PRUEBA { get; set; }
+        public string CL_PRUEBA { get; set; }
+        public string NB_PRUEBA { get; set; }
+        public int ID_CUESTIONARIO { get; set; }
+        public short NO_TIEMPO_PRUEBA { get; set; }
+    }
+}
