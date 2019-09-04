3411a0469812d915de63213cc3097b85aa3ead73
fpadillasigein <51723090+fpadillasigein@users.noreply.github.com>
2019-09-04 09:35:50 -0500

    Francisco Padilla
    Clase encapsulada para PDE

---
+++ SistemaSIGEIN/SIGE.Entidades/Administracion/E_EMPLEADOS_GENERA_CONTRASENA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIGE.Entidades.Administracion
+{
+    public class E_EMPLEADOS_GENERA_CONTRASENA
+    {
+        public string ID_EMPLEADO { get; set; }
+        public string NB_EMPLEADO { get; set; }
+        public string NB_PATERNO { get; set; }
+        public string CORREO_ELECTRONICO { get; set; }
+        public int ID_ROL { get; set; }
+        public string ID_Grupo { get; set; }
+        public string ID_USUARIO { get; set; }
+        public string CONTRASENA { get; set; }
+    }
+}
